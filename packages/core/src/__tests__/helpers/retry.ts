86e7668edaa8f849085edec28c1cc8e0d4182494
ShookLyngs <shooklyngs@gmail.com>
2024-01-08 18:07:18 +0800

    test(sdk): expose error details in retryQuery

--- packages/core/src/__tests__/helpers/retry.ts
+++ packages/core/src/__tests__/helpers/retry.ts
@@ -3,13 +3,15 @@ import { retryWork } from '../../helpers';
 export async function retryQuery<T>(getter: () => T | Promise<T>): Promise<T> {
   const work = await retryWork({
     getter,
-    retry: 6,
+    retry: 8,
     interval: 10000,
   });
 
   if (!work.success) {
     if (work.errors.length > 0) {
-      throw work.errors.pop();
+      throw new Error(`RetryWork failed for ${work.retries} times`, {
+        cause: work.errors.pop(),
+      });
     } else {
       throw new Error(`RetryWork failed with no error for ${work.retries} times`);
     }
