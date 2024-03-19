0d5e7a96d6fc102da72806564c3941f6e785d4d0
lee920217 <francislee920217@gmail.com>
2024-03-20 01:54:43 +0800

    pass private to account generate function directly

--- packages/core/src/__tests__/shared/env.ts
+++ packages/core/src/__tests__/shared/env.ts
@@ -35,6 +35,6 @@ export const TEST_ENV = {
 };
 
 export const TEST_ACCOUNTS = {
-  CHARLIE: createDefaultLockAccount(TEST_VARIABLES.accounts.charlie, config),
-  ALICE: createDefaultLockAccount(TEST_VARIABLES.accounts.alice, config),
+  CHARLIE: createDefaultLockAccount('0xd6013cd867d286ef84cc300ac6546013837df2b06c9f53c83b4c33c2417f6a07', config),
+  ALICE: createDefaultLockAccount('0xfd686a48908e8caf97723578bf85f746e1e1d8956cb132f6a2e92e7234a2a245', config),
 };
