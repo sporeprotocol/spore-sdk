9a5dcbc1d05b6ca59d1e62ad215b96027a086d05
lee920217 <francislee920217@gmail.com>
2024-03-20 02:34:05 +0800

    update private key

--- packages/core/src/__tests__/shared/env.ts
+++ packages/core/src/__tests__/shared/env.ts
@@ -35,6 +35,6 @@ export const TEST_ENV = {
 };
 
 export const TEST_ACCOUNTS = {
-  CHARLIE: createDefaultLockAccount(TEST_VARIABLES.accounts.charlie, config),
-  ALICE: createDefaultLockAccount(TEST_VARIABLES.accounts.alice, config),
+  CHARLIE: createDefaultLockAccount('0xc153ee57dc8ae3dac3495c828d6f8c3fef6b1d0c74fc31101c064137b3269d6d', config),
+  ALICE: createDefaultLockAccount('0x49aa6d595ac46cc8e1a31b511754dd58f241a7d8a6ad29e83d6b0c1a82399f3d', config),
 };
